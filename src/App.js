b674b4ce7fedf1ffb976ad0e78195918c1b14079
kmalay <kmalay@blackhawkenterprise.com>
2019-07-05 14:17:22 -0400

    Updates.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          This is my new text.
+          This is some new text.
         </p>
         <a
           className="App-link"
