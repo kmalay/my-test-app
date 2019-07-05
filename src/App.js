1a4a25fcb96220ac7a907f668861f5c9bcbf672a
kmalay <kmalay@blackhawkenterprise.com>
2019-07-05 14:13:37 -0400

    Fix component text.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          This is my new sample project.
+          This is my text.
         </p>
         <a
           className="App-link"
